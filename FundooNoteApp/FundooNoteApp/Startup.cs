05928d9fa11262a71bcec150ce74ca9db5e9cfbc
rohantare03 <rohatare123@gmail.com>
2022-08-05 19:00:16 +0530

    [Rohan] Add : Adding Redis Cache to Notes Table

--- FundooNoteApp/FundooNoteApp/Startup.cs
+++ FundooNoteApp/FundooNoteApp/Startup.cs
@@ -91,6 +91,12 @@ namespace FundooNoteApp
             services.AddTransient<ILabelBL, LabelBL>();
             services.AddTransient<ILabelRL, LabelRL>();
 
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = "localhost:6379";
+            });
+            services.AddMemoryCache();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
